cb90ae0486722aade91377efa8ee37e09dfbd5e1
NatthawutMark <natthawut.k@iconsolutions.co.th>
2025-12-22 22:26:34 +0700

    แก้เรื่อง startPath และ config นิดหน่อย

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return[
+        {
+            source: '/',
+            destination: '/home',
+            permanent: false,
+        },
+    ]
+  },
+  
+//   basePath: "/home",
 };
 
 export default nextConfig;
